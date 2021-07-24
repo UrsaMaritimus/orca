6dd8bd1f7c67a924af4c985a5fbd890c045d468a
Ursa M <maritim.ursa@gmail.com>
2021-07-24 12:21:22 -0700

    Updated redirect to vaults

--- apps/ursa/pages/index.tsx
+++ apps/ursa/pages/index.tsx
@@ -1,10 +1,11 @@
 import React, { FC } from 'react';
-
+import { GetServerSideProps } from 'next';
 import { experimentalStyled as styled } from '@material-ui/core/styles';
 
 import { Page } from '@ursa/components/page';
 import { AppPage } from '@ursa/pages/app';
 import { AppLayout } from '@ursa/layout/app';
+import { routes } from '@ursa/shared/base';
 
 const RootStyle = styled(Page)((theme) => ({
   root: {},
@@ -21,6 +22,17 @@ const App: FC = () => {
       </AppLayout>
     </RootStyle>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  if (context.res) {
+    context.res.writeHead(302, { Location: routes.APP.VAULTS.USER });
+    context.res.end();
+
+    return {
+      props: {},
+    };
+  }
 };
 
 export default App;
