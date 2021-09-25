6ac55fa5e7503ea6dc5749e7e2928018f8ec0c3f
Ursa M <maritim.ursa@gmail.com>
2021-09-24 19:46:38 -0700

    Update discord link

--- libs/shared/base/src/lib/routes.ts
+++ libs/shared/base/src/lib/routes.ts
@@ -22,7 +22,7 @@ export const routes = {
     TWITTER: 'https://twitter.com/OrcaDAO',
     GITHUB: 'https://github.com/UrsaMaritimus/orca',
     DOCS: 'https://docs.avai.finance/',
-    DISCORD: 'https://discord.gg/JC8tAJN6',
+    DISCORD: 'https://discord.gg/qbA9ZvrmVS',
     SITE:
       process.env.NODE_ENV === 'development'
         ? 'http://localhost:4200/vaults'
