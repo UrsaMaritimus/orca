841cf9ca2b00075b1ab01cd35b6c9f2b74e74051
Ursa <test@test.ca>
2021-09-26 17:19:23 -0700

    Temp commit

--- apps/avai/src/contracts/Vesting.sol
+++ apps/avai/src/contracts/Vesting.sol
@@ -1,10 +1,75 @@
 // SPDX-License-Identifier: agpl-3.0
 pragma solidity ^0.8.0;
 
-import '@openzeppelin/contracts/finance/PaymentSplitter.sol';
+import '@openzeppelin/contracts/utils/Address.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
 
-contract OrcaTeamVesting is PaymentSplitter {
-  constructor(address[] memory payees, uint256[] memory shares_)
-    PaymentSplitter(payees, shares_)
-  {}
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
+
+contract OrcaTeamVesting is Ownable {
+  using SafeERC20 for IERC20;
+
+  event PayeeAdded(address account, uint256 shares);
+  event PaymentReleased(address to, uint256 amount);
+  event PaymentReceived(address from, uint256 amount);
+
+  uint256 private _totalShares;
+  uint256 private _totalReleased;
+
+  mapping(address => uint256) private _shares;
+  mapping(address => uint256) private _released;
+  address[] private _payees;
+
+  IERC20 private orca;
+
+  constructor(
+    address orca_,
+    address[] memory payees,
+    uint256[] memory shares_,
+    uint256 _start,
+    uint256 _duration,
+    bool _revocable
+  ) {
+    require(
+      payees.length == shares_.length,
+      'PaymentSplitter: payees and shares length mismatch'
+    );
+    require(payees.length > 0, 'PaymentSplitter: no payees');
+
+    for (uint256 i = 0; i < payees.length; i++) {
+      _addPayee(payees[i], shares_[i]);
+    }
+  }
+
+  /**
+   * @dev The ORCA received will be logged with {PaymentReceived} events. Note that these events are not fully
+   * reliable: it's possible for a contract to receive Ether without triggering this function. This only affects the
+   * reliability of the events, and not the actual splitting of Ether.
+   */
+  function receiveToken(uint256 amount) external {
+    emit PaymentReceived(_msgSender(), amount);
+  }
+
+  /**
+   * @dev Add a new payee to the contract.
+   * @param account The address of the payee to add.
+   * @param shares_ The number of shares owned by the payee.
+   */
+  function _addPayee(address account, uint256 shares_) private {
+    require(
+      account != address(0),
+      'PaymentSplitter: account is the zero address'
+    );
+    require(shares_ > 0, 'PaymentSplitter: shares are 0');
+    require(
+      _shares[account] == 0,
+      'PaymentSplitter: account already has shares'
+    );
+
+    _payees.push(account);
+    _shares[account] = shares_;
+    _totalShares = _totalShares + shares_;
+    emit PayeeAdded(account, shares_);
+  }
 }
