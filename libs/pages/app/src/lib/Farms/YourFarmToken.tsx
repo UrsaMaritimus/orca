8584c4b742c57a9429cd533eef2a3b84ac68b227
Ursa M <maritim.ursa@gmail.com>
2021-10-01 16:26:12 -0700

    Update front page orca stat

--- libs/pages/app/src/lib/Farms/YourFarmToken.tsx
+++ libs/pages/app/src/lib/Farms/YourFarmToken.tsx
@@ -156,7 +156,7 @@ export const YourFarmInfo: FC = () => {
                 .isZero()
             }
           >
-            {!shouldFetch ? 'Connect' : 'Claim'}
+            {!shouldFetch ? 'Connect' : 'Claim (All Farms)'}
           </LoadingButton>
         </Grid>
       </Grid>
