e1ae2a9bc5ff888b9d4f153083de086547bb7c2b
Ursa <test@test.ca>
2021-10-29 14:52:25 -0700

    Fix vault issue

--- libs/shared/funcs/src/lib/price.ts
+++ libs/shared/funcs/src/lib/price.ts
@@ -21,13 +21,18 @@ export const bankPrice = () => {
 // swr
 export const allBankPrices = () => {
   return async (_: string, library: Web3Provider, chainId: number) => {
-    return Promise.all(
+    const results = await Promise.all(
       VaultTypes.map(async (vaultType) => {
+        console.log(vaultType);
         const vault = getVault(vaultType.name, library, chainId);
-        const price = await vault.getPriceSource();
-        const peg = await vault.getPricePeg();
-        return { price, peg, symbol: vaultType.symbol, name: vaultType.name };
+        if (vault) {
+          const price = await vault.getPriceSource();
+          const peg = await vault.getPricePeg();
+          return { price, peg, symbol: vaultType.symbol, name: vaultType.name };
+        }
       })
     );
+
+    return results.filter((n) => n);
   };
 };
