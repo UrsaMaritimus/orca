7955627520808ee1c0ed460714436cd6005c6c0e
Ursa M <maritim.ursa@gmail.com>
2021-12-08 09:32:48 -0800

    ADjust dashboard layout

--- libs/layouts/src/app/DashboardLayout.tsx
+++ libs/layouts/src/app/DashboardLayout.tsx
@@ -1,5 +1,5 @@
-import { useState, FC } from 'react';
-import { styled } from '@mui/material/styles';
+import { ReactNode, useState, FC } from 'react';
+import { Box, alpha, lighten, useTheme } from '@mui/material';
 
 import { MHidden, ThemeSettings } from '@orca/components';
 
@@ -9,43 +9,64 @@ import { MobileAccountInfo } from './Mobile';
 
 // ----------------------------------------------------------------------
 
-const APP_BAR_MOBILE = 64;
-const APP_BAR_DESKTOP = 92;
-
-const RootStyle = styled('div')({
-  display: 'flex',
-  minHeight: '100%',
-  overflow: 'hidden',
-});
-
-const MainStyle = styled('div')(({ theme }) => ({
-  flexGrow: 1,
-  overflow: 'auto',
-  minHeight: '100%',
-  paddingTop: APP_BAR_MOBILE + 24,
-  paddingBottom: theme.spacing(10),
-  [theme.breakpoints.up('lg')]: {
-    paddingTop: APP_BAR_DESKTOP + 24,
-    paddingLeft: theme.spacing(2),
-    paddingRight: theme.spacing(2),
-  },
-}));
+interface ExtendedSidebarLayoutProps {
+  children?: ReactNode;
+}
 
 // ----------------------------------------------------------------------
 
-const DashboardLayout: FC = ({ children }) => {
+const DashboardLayout: FC<ExtendedSidebarLayoutProps> = ({ children }) => {
+  const theme = useTheme();
   const [openNav, setOpenNav] = useState(false);
 
   return (
-    <RootStyle>
+    <Box
+      sx={{
+        flex: 1,
+        height: '100%',
+
+        '.MuiPageTitle-wrapper': {
+          background:
+            theme.palette.mode === 'dark'
+              ? theme.colors.alpha.trueWhite[5]
+              : theme.colors.alpha.white[50],
+          marginBottom: `${theme.spacing(4)}`,
+          boxShadow:
+            theme.palette.mode === 'dark'
+              ? '0 1px 0 ' +
+                alpha(lighten(theme.colors.primary.main, 0.7), 0.15) +
+                ', 0px 2px 4px -3px rgba(0, 0, 0, 0.2), 0px 5px 12px -4px rgba(0, 0, 0, .1)'
+              : '0px 2px 4px -3px ' +
+                alpha(theme.colors.alpha.black[100], 0.1) +
+                ', 0px 5px 12px -4px ' +
+                alpha(theme.colors.alpha.black[100], 0.05),
+        },
+      }}
+    >
       <TopBar onOpenNav={() => setOpenNav(true)} />
       <NavBar onCloseNav={() => setOpenNav(false)} isOpenNav={openNav} />
-      <MainStyle>{children}</MainStyle>
-      <ThemeSettings />
+      <Box
+        sx={{
+          position: 'relative',
+          zIndex: 5,
+          display: 'block',
+          flex: 1,
+          pt: `${theme.header.height}`,
+          [theme.breakpoints.up('lg')]: {
+            ml: `${theme.sidebar.width}`,
+          },
+        }}
+      >
+        <Box display="block">
+          {children}
+          <ThemeSettings />
+        </Box>
+      </Box>
+
       <MHidden width="smUp">
         <MobileAccountInfo />
       </MHidden>
-    </RootStyle>
+    </Box>
   );
 };
 
