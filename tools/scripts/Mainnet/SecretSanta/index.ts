b3f03fc171efd814c4782e43103686416529e798
Ursa M <maritim.ursa@gmail.com>
2021-12-14 23:21:36 -0800

    Add secret santa list

---
+++ tools/scripts/Mainnet/SecretSanta/index.ts
@@ -0,0 +1,33 @@
+import { GraphQLClient, gql } from 'graphql-request';
+
+const GET_SECRET_SANTA_WINNER = gql`
+  query podUsers {
+    podUsers {
+      id
+      xorcaBurned
+      xorcaMinted
+    }
+  }
+`;
+
+const handler = async () => {
+  const client = new GraphQLClient(
+    'https://api.thegraph.com/subgraphs/name/ursamaritimus/orca-dao'
+  );
+  const result = await client.request(GET_SECRET_SANTA_WINNER);
+
+  const users = result.podUsers;
+  const userHasXOrca = users.filter(
+    (val) => val.xorcaMinted - val.xorcaBurned > 0
+  );
+
+  const winner = userHasXOrca[Math.floor(Math.random() * userHasXOrca.length)];
+  console.log(winner);
+};
+
+handler()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
