dbfa9598f487c39e751f5cb1f6fe0cb68ab44be6
Ursa M <maritim.ursa@gmail.com>
2021-12-15 17:07:39 -0800

    Fix img

--- libs/components/src/logo/Logo.tsx
+++ libs/components/src/logo/Logo.tsx
@@ -1,7 +1,5 @@
 import { FC } from 'react';
 import { Box } from '@mui/material';
-import Image from 'next/image';
-
 type Props = {
   sx?: Record<string, unknown>;
 };
@@ -9,12 +7,11 @@ type Props = {
 const Logo: FC<Props> = ({ sx, ...other }) => {
   return (
     <Box sx={sx}>
-      <Image
+      <img
         alt="logo"
         src="/static/brand/logo_solo_santa.png"
         height={40}
         width={40}
-        quality={100}
         {...other}
       />
     </Box>
